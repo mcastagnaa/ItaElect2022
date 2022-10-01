ae3c8a13c6eefbafc2d14e6518493b59081dd96a
mcastagnaa <matteo.castagna@gmail.com>
2022-10-01 23:30:55 +0100

    Initial commit

---
+++ script.R
@@ -0,0 +1,49 @@
+library(dplyr)
+library(tidyr)
+library(janitor)
+library(openxlsx)
+library(ggplot2)
+
+rm(list = ls())
+
+PercRedCitt <- read.xlsx("PercRedPensCittadinanza.xlsx") %>%
+  mutate(Provincia = toupper(Provincia))
+
+Elect2022 <- read.csv("Politiche2022_Scrutini_Camera_Italia.csv", sep = ";") %>%
+  select(Provincia = PROVINCIA,
+         Comune = COMUNE,
+         Collegio = COLLEGIO.UNINOMINALE,
+         Cognome = COGNOME,
+         Nome = NOME,
+         Partito = LISTA,
+         Votanti = VOTANTI.TOTALI,
+         Elettori = ELETTORI.TOTALI,
+         Voti = VOTI.CANDIDATO)
+
+Voters <- Elect2022 %>%
+  group_by(Provincia, Comune, Collegio) %>%
+  summarise(Votanti = first(Votanti)) %>%
+  group_by(Provincia, Comune) %>%
+  summarise(Votanti = sum(Votanti)) %>%
+  group_by(Provincia) %>%
+  summarise(Votanti = sum(Votanti))
+
+Votes <- Elect2022 %>%
+  group_by(Provincia, Comune, Cognome, Nome) %>%
+  summarise(Partito = first(Partito),
+            Voti = first(Voti)) %>%
+  group_by(Partito, Provincia) %>%
+  summarise(Voti = sum(Voti))
+
+dataset <- Votes %>%
+  left_join(Voters, by = "Provincia") %>%
+  mutate(percVotes = Voti/Votanti) %>%
+  full_join(PercRedCitt, by = "Provincia")
+
+dataset %>%
+  filter(Partito == "MOVIMENTO 5 STELLE") %>%
+  ggplot(aes(x = PercRdC, y = percVotes)) +
+  geom_smooth(formula = y ~ x, method = "lm") +
+  geom_point()
+
+summary(lm(data = dataset, formula = percVotes ~ PercRdC))
